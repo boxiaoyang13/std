8b72b68cf72b2a58d673a99b1a7c16eb53d7735e
system <system@qq.com>
2016-12-18 13:23:58 +0800

    ch7 over

--- ch7/ostream.cpp
+++ ch7/ostream.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 #include <iterator>
+#include <vector>
+#include <algorithm>
+#include <string>
 
 using namespace std;
 
@@ -8,6 +11,29 @@ int main(int argc, char **argv)
     ostream_iterator<int> writeIt(cout, "\n");
 
     *writeIt = 10;
+    writeIt++;
+    *writeIt = 20;
+
+    vector<int> coll;
+    for(int i = 0; i < 20; i++)
+        coll.push_back(i);
+
+    copy(coll.begin(), coll.end(),
+         ostream_iterator<int>(cout, " "));
+    cout << endl;
+
+    istream_iterator<string> intIt(cin);
+    istream_iterator<string> intEOF;
+    ostream_iterator<string> strIt(cout);
+    while(intIt != intEOF)
+    {
+        advance(intIt, 2);
+
+        if(intIt != intEOF)
+           *strIt++ = *intIt++;
+
+
+    }
 
     return 0;
 }
